ceb7a08d9dd2565d58dad29b85d69429bfb06024
CooLNuanfeng <1634898698@qq.com>
2017-10-20 14:11:32 +0800

    vue-dialog

---
+++ component/dialog.vue
@@ -0,0 +1,79 @@
+<template lang="html">
+    <div class="fl-dialog-mask" v-if="mshow&&cshow">
+        <div class="fl-dialog">
+            <a v-if='pos=="top"' href="javascript:;" class="fl-close" @click="close">&times;</a>
+            <slot name="dialog-header"></slot>
+            <slot name="dialog-body"></slot>
+            <slot name="dialog-footer"></slot>
+            <a v-if='pos=="btm"' href="javascript:;" class="fl-dialog-close" @click="close"></a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'dialog',
+    data(){
+        return {
+            pos : this.closepos,
+            cshow : true
+        }
+    },
+    props : ['show','closepos'],
+    computed: {
+      mshow: function () {
+          return this.show
+      }
+    },
+    methods: {
+        close(){
+            this.cshow = false;
+            this.$emit('closecb');
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+$opacity: .7;
+.fl-dialog-mask{
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 200;
+    background: rgba(0,0,0,$opacity);
+}
+.fl-dialog{
+    position: absolute;
+    left: .75rem;
+    right: .75rem;
+    top: 50%;
+    transform: translateY(-50%);
+}
+.fl-close{
+    display: block;
+    position: absolute;
+    right: .15rem;
+    top: .15rem;
+    width: .4rem;
+    height: .4rem;
+    color: #666;
+    font-size: .4rem;
+    line-height: .4rem;
+    text-align: center;
+}
+.fl-dialog-close{
+    position: absolute;
+    width: .6rem;
+    height: .6rem;
+    display: block;
+    bottom: -1.2rem;
+    left: 50%;
+    margin-left: -.3rem;
+    background: url(http://static2.51fanli.net/webapp/images/coupon/close.png) no-repeat;
+    background-size: 100% 100%;
+
+}
+</style>
---
+++ demo.js
@@ -0,0 +1,37 @@
+import vDialog from '../components/dialog/dialog.vue';
+
+var app = new Vue({
+    el : '#app',
+    data : {
+        firstdialog : false,
+        ruledialog : false
+    },
+    components: {
+        'v-dialog' : vDialog
+    },
+    created(){
+        this.firstdialog = false;
+        this.ruledialog = true;
+    },
+    mounted(){
+        var vm = this;
+        $.ajax({
+            url : listUrl,
+            type : 'post',
+            dataType : 'json'
+        }).done(function(res){
+            console.log(res);
+            vm.goods = res.list;
+            vm.lists = res.first;
+        });
+    },
+    methods: {
+        firstclose(){
+            this.firstdialog = false;
+            console.log('aa',this.firstdialog);
+        },
+        firstcbclose(){
+            // console.log('close form child');
+        }
+    }
+});
